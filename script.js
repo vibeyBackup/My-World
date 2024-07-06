bcb3a31ed00ecaecf88bab653f5c556ad05d7717
vibeyBackup <uno24124@gmail.com>
2024-07-06 22:43:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,49 +10,49 @@ function captureSelectedOption() {
   var audioElement = document.createElement("audio"); // Create audio element
 
   // Define the audio source for 1080p selection
-  audioElement.src = "../myWorld/assets/myLove.mp3";
+  audioElement.src = "./assets/myLove.mp3";
   audioElement.autoplay = true;
   audioElement.loop = true;
   audioElement.volume = 1; // Adjust volume as needed
 
   // Set the media source and display text based on the selected value
   if (selectedValue === "144p") {
-    imageElement.src = "../myWorld/assets/world144.jpg";
+    imageElement.src = "./assets/world144.jpg";
     imageElement.classList.remove("hidden");
     videoElement.classList.add("hidden");
     pageTitleElement.classList.add("hidden");
     mediaContainer.classList.remove("hidden");
     audioElement.pause(); // Pause music for non-1080p selections
   } else if (selectedValue === "240p") {
-    imageElement.src = "../myWorld/assets/world240.png";
+    imageElement.src = "./assets/world240.png";
     imageElement.classList.remove("hidden");
     videoElement.classList.add("hidden");
     pageTitleElement.classList.add("hidden");
     mediaContainer.classList.remove("hidden");
     audioElement.pause(); // Pause music for non-1080p selections
   } else if (selectedValue === "360p") {
-    imageElement.src = "../myWorld/assets/world360.jpg";
+    imageElement.src = "./assets/world360.jpg";
     imageElement.classList.remove("hidden");
     videoElement.classList.add("hidden");
     pageTitleElement.classList.add("hidden");
     mediaContainer.classList.remove("hidden");
     audioElement.pause(); // Pause music for non-1080p selections
   } else if (selectedValue === "480p") {
-    imageElement.src = "../myWorld/assets/world480.png";
+    imageElement.src = "./assets/world480.png";
     imageElement.classList.remove("hidden");
     videoElement.classList.add("hidden");
     pageTitleElement.classList.add("hidden");
     mediaContainer.classList.remove("hidden");
     audioElement.pause(); // Pause music for non-1080p selections
   } else if (selectedValue === "720p") {
-    imageElement.src = "../myWorld/assets/world720.jpg";
+    imageElement.src = "./assets/world720.jpg";
     imageElement.classList.remove("hidden");
     videoElement.classList.add("hidden");
     pageTitleElement.classList.add("hidden");
     mediaContainer.classList.remove("hidden");
     audioElement.pause(); // Pause music for non-1080p selections
   } else if (selectedValue === "1080p") {
-    videoElement.src = "../myWorld/assets/world1080.mp4";
+    videoElement.src = "./assets/world1080.mp4";
     videoElement.classList.remove("hidden");
     imageElement.classList.add("hidden");
     pageTitleElement.classList.remove("hidden");
